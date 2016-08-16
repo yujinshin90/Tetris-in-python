5231d1f903e610bb364d04d2e8e0e73cbc95b5b9
Evonne Ng <evonne_ng@utexas.edu>
2016-08-16 13:41:39 -0700

    added rotation and collision check

--- Tetris.py
+++ Tetris.py
@@ -8,7 +8,7 @@ rows =	22
 maxfps = 30
 
 #define each of the tetris shape objects
-tetris_shapes = [
+tetris_piece = [
 	[[1, 1, 1],
 	 [0, 1, 0]],
 	
@@ -36,3 +36,20 @@ def new_board():
 				for y in xrange(rows)]
 	board += [[0 for x in xrange(cols)]]
 	return board
+
+#define helper methods used in moving the pieces
+def rotate_clock(piece):
+	return [ [ piece[y][x]
+			for y in xrange(len(piece)) ]
+		for x in xrange(len(piece[0]) - 1, -1, -1) ]
+
+def is_collsion(board, piece, offset):
+	offx, offy = offset
+	for y, row in enumerate(piece):
+		for x, cell in enumerate(row):
+			try:
+				if cell and board[ y + offy ][ x + offx ]:
+					return True
+			except IndexError:
+				return True
+	return False
