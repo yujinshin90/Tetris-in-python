c9b7440e968dc7f754336338e977bc4b803dee2b
evonne <evonne_ng@utexas.edu>
2016-09-07 10:28:31 -0500

    added v1 of tetris

--- Tetris.py
+++ Tetris.py
@@ -251,11 +251,26 @@ class Tetris_App(object):
                                      (self.board_width+1, self.height-1))
                     self.disp_msg("Next: ", (self.board_width+cell_size, 2))
 
-        self.disp_msg("Score: %d\n\nLevel: %d\nLines: %d" %(self.score, self.level, self.lines),
-                      (self.board_width+cell_size, cell_size*5))
-        self.draw_matrix(self.bground_grid, (0,0))
-        self.draw_matrix(self.board, (0,0))
-        self.draw_matrix(self.piece, (self.piece_x, self.piece_y))
+                    self.disp_msg("Score: %d\n\nLevel: %d\nLines: %d" %(self.score, self.level, self.lines),
+                        (self.board_width+cell_size, cell_size*5))
+                    self.draw_matrix(self.bground_grid, (0,0))
+                    self.draw_matrix(self.board, (0,0))
+                    self.draw_matrix(self.piece, (self.piece_x, self.piece_y))
+                    self.draw_matrix(self.piece, (self.piece_x, self.piece_y))
+                    self.draw_matrix(self.next_piece, (cols+1, 2))
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.USEREVENT+1:
+                    self.drop(False)
+                elif event.type == pygame.QUIT:
+                    self.quit()
+                elif event.type == pygame.KEYDOWN:
+                    for key in key_actions:
+                        if event.key == eval("pygame.K_" + key):
+                            key_actions[key]()
+
+            cpu.tick(maxfps)
 
 if __name__ == "__main__":
     App = Tetris_App()
